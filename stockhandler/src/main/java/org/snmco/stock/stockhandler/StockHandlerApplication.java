76bef519d3a5a63a49803797659458df7c6a9aa7
mmcordoba <manuelmaciascordoba@gmail.com>
2015-03-15 19:04:09 +0000

    stockhandler skeleton

---
+++ stockhandler/src/main/java/org/snmco/stock/stockhandler/StockHandlerApplication.java
@@ -0,0 +1,12 @@
+package org.snmco.stock.stockhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockHandlerApplication.class, args);
+    }
+}
